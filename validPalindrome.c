5886b372a70101c2e1b7fd5178df0d5ace0a91cd
isroben <robenthapaaa@gmail.com>
2026-03-23 21:30:06 +0545

    WAP to take if the supplied string is palindrome or not without using strrev() function.

---
+++ validPalindrome.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+#include<ctype.h>
+
+bool isPalindrome(char str[]){ // Two pointer approach
+    int n = strlen(str);
+    int st = 0, end = n-1;
+
+    while(st < end){
+        if(tolower(str[st] != tolower(str[end]))){
+            return false;
+        }
+        st++; end--;
+    }
+    return true;
+}
+
+int main(){
+    char str[] = "cac";
+
+    bool result = isPalindrome(str);
+
+    if(result){
+        printf("valid palindrome!");
+    }
+    else{
+        printf("Not a valid palindrome!");
+    }
+
+    return 0;
+}
