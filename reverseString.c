1eb7fdae772951c0722bb849b0b02a856b33865e
isroben <robenthapaaa@gmail.com>
2026-03-23 21:17:03 +0545

    WAP to reverse string without using strrev() function

---
+++ reverseString.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[] = "Hello World!";
+    int n = strlen(str);
+
+    for(int i=0; i<n/2; i++){
+        char temp = str[i];
+        str[i] = str[n-i-1];
+        str[n-i-1] = temp;
+    }
+
+    printf("Reversed string %s", str);
+
+    return 0;
+}
