d42fbcfb92d03e6dcc94445c0ca0c4c3a8f1cefe
isroben <robenthapaaa@gmail.com>
2026-03-23 21:49:38 +0545

    printing the pattern RRBCA

--- RRCAMPUS_patterns.c
+++ RRCAMPUS_patterns.c
@@ -1,16 +1,39 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
-    char str[] = "RRCAMPUS";
+void RRCAMPUS(char str[]){
     int n = strlen(str);
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<=i; j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             printf("%c", str[j]);
         }
         printf("\n");
     }
+    return;
+}
+
+void RRBCA(char str[]){
+    int n = strlen(str);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            printf("%c", str[j]);
+        }
+        printf("\n");
+    }
+    return;
+}
+
+int main()
+{
+    char str1[] = "RRCAMPUS";
+    RRCAMPUS(str1);
+
+    char str2[] = "RRBCA";
+    RRBCA(str2);
 
     return 0;
 }
