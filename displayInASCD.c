2b18e32ed41fa5f644f8826a41a8cc4260759f9c
isroben <robenthapaaa@gmail.com>
2026-03-23 19:46:42 +0545

    WAP to store n num of elements and display them in ascending order

---
+++ displayInASCD.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void insertionSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        int curr = arr[i];
+        int prev = i-1;
+
+        while(prev >= 0 && arr[prev] > curr){
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        arr[prev+1] = curr;
+    }
+    return;
+}
+
+int main(){
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+ 
+
+    int arr[n];
+
+    printf("Enter the elements of array: ");
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
