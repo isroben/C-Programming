d598d2e864a58004486346ef131e1188608c3389
isroben <robenthapaaa@gmail.com>
2026-03-28 10:40:12 +0545

    fixed string functions

--- structures.c
+++ structures.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
+#include<string.h>
 
 struct Student {
     int roll;
-    char name[20];
-    char address[20];
+    char name[25];
+    char address[25];
 };
 
 int main(){
-    struct Student st[10];
+    struct Student st[2];
 
     for(int i=0; i<10; i++){
         printf("\nEnter the roll no, name and adress one by one: ");
-        scanf(" %d %s %s", &st[i].roll, st[i].name, st[i].address);
+        scanf(" %d", &st[i].roll);
+        
+        getchar(); // buffering
+        
+        printf("Enter the Name of Student: ");
+        fgets(st[i].name, 25, stdin);
+        printf("Enter the address of student: ");
+        fgets(st[i].address, 25, stdin);
     }
 
     for(int j=0; j<10; j++){
