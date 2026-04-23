94a2cb5229c9051462b0a2f6bfecd7579e4629be
isroben <robenthapaaa@gmail.com>
2026-04-23 12:38:52 +0545

    Pointer arithematic

---
+++ pointereverse.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    int *ptr = arr;
+
+    printf("Enter the elements of array: ");
+    for(int i=0; i<n; i++){
+        scanf("%d", (ptr+i));
+    }
+
+    printf("Array in reverse order: ");
+    for(int i=0; i<n; i++){
+        printf("%d ", *(ptr+(n-i-1)));
+    }
+
+    return 0;
+}
